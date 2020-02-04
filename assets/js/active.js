c3485c168fdc1935c24be959a727fa69b70a1036
samMzi <sam.mezzi21@gmail.com>
2020-02-04 10:44:45 +0100

    Add files via upload

---
+++ assets/js/active.js
@@ -0,0 +1,19 @@
+// Get the container element
+//var btnContainer = document.getElementById("Mydiv");
+
+// Get all buttons with class="btn" inside the container
+//var btns = btnContainer.getElementsByClassName("nav-link ");
+
+// Loop through the buttons and add the active class to the current/clicked button
+/*for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}*/
+
+$('li a').click(function(){
+  	$('li a').removeClass('active');
+	$(this).addClass('active');
+})
